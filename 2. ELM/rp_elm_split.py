eaa62d8572861713835fab50e08b0652ad1e6244
dave <david.borges@protonmail.com>
2019-04-17 16:22:51 -0300

    Término da implementação da superfície de separação para ELM sobre a base twomoons

--- 2. ELM/rp_elm_split.py
+++ 2. ELM/rp_elm_split.py
@@ -20,13 +20,8 @@ with open('twomoons.dat') as twomoons:
 dataset = numpy.array(samples)
 # Preenchimento do vetor de amostras X e suas classes Y
 X = dataset[:,0:2]
-# Representação de classes (e.g. [0 1])
-Y_ = dataset[:,2:]
-Y_ = [0 if y == -1 else 1 for y in Y_]
-Y_ = numpy.array(Y_)
-# Representação one-hot-encoding (e.g. [[1 0], [0 1]])
-Y = [[1,0] if y == 0 else [0,1] for y in Y_]
-Y = numpy.array(Y)
+# Representação de classes (e.g. [1 -1])
+Y = dataset[:,2:]
 
 # Função de normalização
 def zscore(X):
@@ -34,9 +29,8 @@ def zscore(X):
     X = X / numpy.std(X, axis=0, ddof=1)
     return X
 
-# Função sigmoide
-def sigmoid(x):
-    return 1 / (1 + numpy.exp(-x))
+def tanh(x):
+    return numpy.tanh(x)
 
 # Normalização das amostras apenas se for passado parâmetro -n
 if args.normalize:
@@ -65,7 +59,7 @@ def elm(X_train, Y_train, X_test, q=10):
     # Adição do bias à matriz X_train
     X_train = numpy.concatenate((numpy.ones((1,N)), X_train))
     # Passagem pela camada oculta e adição de bias a Z
-    Z = sigmoid(numpy.dot(W, X_train))
+    Z = tanh(numpy.dot(W, X_train))
     Z = numpy.concatenate((numpy.ones((1,N)), Z))
     # Cálculo dos pesos entre a camada oculta e camada de saída
     M = numpy.dot((numpy.dot(D,Z.T)),(numpy.linalg.pinv(numpy.dot(Z,Z.T))))
@@ -79,24 +73,45 @@ def elm(X_train, Y_train, X_test, q=10):
     # Adição do bias
     X_test = numpy.concatenate((numpy.ones((1,N_)), X_test))
     # Passagem das amostras de teste pela camada oculta e adição do bias
-    Z_ = sigmoid(numpy.dot(W, X_test))
+    Z_ = tanh(numpy.dot(W, X_test))
     Z_ = numpy.concatenate((numpy.ones((1,N_)), Z_))
     # Cálculos dos valores da camada de saída
-    D_ = sigmoid(numpy.dot(M, Z_))
-    # Cálculo do índice do neurônio que proporciona a maior ativação na saída
-    y = numpy.argmax(D_, axis=0)
+    D_ = tanh(numpy.dot(M, Z_))
+    # Resultado da classificação
+    for d in D_[0]:
+        if d > 0: y.append(1)
+        else: y.append(-1)
+    y = numpy.array(y)
     # Fim da contagem de tempo e armazenamento do tempo transcorrido
     t_classification.append(time.time() - checkpoint)
-    return y, s
+    # Cálculo da superfície de separação
+    # Criação de uma grade sobre a superfície dos dados
+    x_0 = numpy.arange(-1, 8.5, 0.01)
+    x_1 = numpy.arange(0, 7, 0.01)
+    # Iteração sobre os pontos da grade
+    boundary = list()
+    for x0 in x_0:
+        for x1 in x_1:
+            # Classificação dos pontos da grade
+            x = numpy.array([[1],[x0],[x1]])
+            z = tanh(numpy.dot(W,x))
+            z = numpy.concatenate((numpy.ones((1,1)), z))
+            d = tanh(numpy.dot(M,z))
+            # Caso a classificação seja incerta, registra os pontos
+            # da grade que produziram a incerteza
+            if abs(d) < 0.01:
+                boundary.append([x0, x1])
+    boundary = numpy.array(boundary)
+    return y, boundary
 
 # Realiza a inferência
-y, s = elm(X, Y, X, q=args.q)
+y, b = elm(X, Y, X, q=args.q)
 
 # Total de amostras
 total = len(X)
 
 # Realiza a contagem de sucessos
-success = sum(y == Y_)
+success = sum(y == Y.T[0])
 
 # Cálculo e impressão do resultado da validação
 result = 100*(success/total)
@@ -106,6 +121,8 @@ print('%.2f %%' % (result))
 print('Tempo médio de treinamento: %f us' % (10**6*numpy.mean(t_train)))
 print('Tempo médio de classificação: %f us' % (10**6*numpy.mean(t_classification)))
 
-plt.scatter(X[:,0], X[:,1], c=Y_, alpha=0.5)
-plt.plot(s[:,0], s[:,1])
+# Scatter plot dos dados de Two Moons
+plt.scatter(X[:,0], X[:,1], c=Y.T[0], alpha=0.5)
+# Plot da superfície de separação
+plt.plot(b[:,0], b[:,1], 'red')
 plt.show()
